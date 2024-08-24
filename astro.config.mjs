2cf55b4e50a5f3bc7298596fa746ebd7e2b1ac22
Ajdin <ajdin_sah@hotmail.com>
2024-08-24 23:13:20 +0200

    astro aws amplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import awsAmplify from 'astro-aws-amplify';
 import { defineConfig } from 'astro/config';
 
 export const DEFAULT_LOCALE = 'en';
@@ -7,6 +8,7 @@ export const AVAILABLE_LOCALES = ['en', 'de'];
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: awsAmplify(),
   integrations: [
     tailwind(),
     react({
